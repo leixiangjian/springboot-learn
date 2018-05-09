36e91e09c3c79ba1e50c9a726ec78fb5e548e54a
leixiangjian <leixiangjian@bolemy.com>
2018-05-09 16:37:23 +0800

    commit;

--- springboot-configuration/src/main/java/com/springboot/learn/configuration/ConfigApplication.java
+++ springboot-configuration/src/main/java/com/springboot/learn/configuration/ConfigApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
+
+@EnableSwagger2Doc // 开启 Swagger
 @ImportResource({"classpath:applicationContext.xml"})
 @ComponentScan(basePackages= {"com.springboot.learn.configuration.annotationimport.service","com.springboot.learn.configuration.annotationimport","com.springboot.learn.configuration.annotationimport.controller"})
 @SpringBootApplication
