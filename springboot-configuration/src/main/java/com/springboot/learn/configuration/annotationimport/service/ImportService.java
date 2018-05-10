8137c7e51984061196cd45fb0bab9302b05308b4
leixiangjian <leixiangjian@bolemy.com>
2018-05-10 15:52:15 +0800

    commit;

--- springboot-configuration/src/main/java/com/springboot/learn/configuration/annotationimport/service/ImportService.java
+++ springboot-configuration/src/main/java/com/springboot/learn/configuration/annotationimport/service/ImportService.java
@@ -1,16 +1,20 @@
 package com.springboot.learn.configuration.annotationimport.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.springboot.learn.configuration.annotationimport.DirectImport;
 
-//@Component
+@Component
 public class ImportService {
 	@Autowired
 	DirectImport directImport;
 	
+	@Value("${my.name}")
+	String test;
+	
 	public String test() {
-		return String.format("%s:%s", "import-service",directImport.test());
+		return String.format("%s:%s:%s", "import-service",directImport.test(),test);
 	}
 }
